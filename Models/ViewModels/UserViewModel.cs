0ffdf856d8567d31c2275007dfb02ddc0082ffb7
Francesco Ruffolo <frweb@outlook.it>
2022-03-20 17:35:45 +0100

    Nuovo User View Model

---
+++ Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+namespace RCommerce.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public int ID{get;set;}
+        public string Nome{get;set;}
+        public string Cognome{get;set;}
+    }
+}
